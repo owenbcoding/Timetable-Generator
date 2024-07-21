ae9831f4eb3fe48366f0a3172ab52ef445cf6cdf
owenbcoding <eoghankb@gmail.com>
2024-07-21 16:24:55 +0100

    created about page with some content

---
+++ src/about.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About Timetable</title>
+    <link href="./css/style.css" rel="stylesheet">
+</head>
+
+<body>
+    <div>
+        <?php include 'navbar.php'; ?>
+        <div class="about-content">
+            <h1 class="about-title">About</h1>
+            <h2 class="about-title2">Timetable-Generator</h2>
+            <p class="about-text">
+                If you're still a student, you can even use this to manage your own timetable
+            </p>
+        </div>
+        <?php include 'footer.php'; ?>
+    </div>
+</body>
+
+</html>
