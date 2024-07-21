021ad4bed96cf8be0cd80c4faf20729284a84e70
owenbcoding <eoghankb@gmail.com>
2024-07-21 15:43:05 +0100

    commented out php pdo code with try catch

--- src/index.php
+++ src/index.php
@@ -1,14 +1,24 @@
-<?php
-try {
-    $dbh = new PDO(
-        'mysql:host=mariadb;dbname=mydatabase;port=3306',
-        'myuser',
-        'mypassword',
-        [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
-    );
-    echo "Database successfully connected.";
-} catch (PDOException $ex) {
-    echo "Database failed to connect: " . $ex->getMessage();
-}
 
-die(phpinfo());
+<?php include 'home.php'; ?>
+
+<?php
+$dbh = new PDO(
+    'mysql:host=mariadb;dbname=mydatabase;port=3306',
+    'myuser',
+    'mypassword',
+    [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
+);
+
+// try {
+//     $dbh = new PDO(
+//         'mysql:host=mariadb;dbname=mydatabase;port=3306',
+//         'myuser',
+//         'mypassword',
+//         [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
+//     );
+//     echo "Database successfully connected.";
+// } catch (PDOException $ex) {
+//     echo "Database failed to connect: " . $ex->getMessage();
+// }
+
+// die(phpinfo());
