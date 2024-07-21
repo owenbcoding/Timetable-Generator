ed41ab277fb106afcd8c63a79b3d159465121efb
owenbcoding <eoghankb@gmail.com>
2024-07-21 15:42:46 +0100

    created home page

---
+++ src/home.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Timetable-Generator</title>
+    <meta name="description" content="..." />
+    <meta name="keywords" content="..." />
+    <link href="./css/style.css" rel="stylesheet">
+</head>
+
+<body>
+    <header>
+        <nav>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Contact</a></li>
+        </nav>
+    </header>
+    <h1>Timetable-Generator</h1>
+    <main class="main-container">
+
+    </main>
+</body>
+
+</html>
